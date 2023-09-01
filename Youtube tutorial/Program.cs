fd6aff8cd4d4cd01e5f9c01d9bca7cb9b1ae44f0
Steffebresse <stefan.kureljusic@hotmail.com>
2023-09-01 09:38:15 +0200

    Add project files.

---
+++ Youtube tutorial/Program.cs
@@ -0,0 +1,52 @@
+﻿
+using System;
+
+
+public class Youtube
+{
+
+
+
+    static void Main(string[] args)
+    {
+        // 2
+
+        int num1 = 5;
+        
+        float fA = 5.5f;
+
+        decimal dA = 4633672367236237;
+
+        // 3
+
+        string f = "3";
+
+        int d = int.Parse(f);
+
+        // 4
+        //  Det man gör är att man blandar typer te.x string och var, med $ 
+
+
+        // 5
+
+        string s = Console.ReadLine();
+        decimal _tal = decimal.Parse(s);
+
+        Console.WriteLine(_tal*10);
+
+        // 6
+
+
+
+
+
+
+
+
+
+
+    }
+
+   
+
+}
